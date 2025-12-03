cf81e6e09a5e9dd8ca0152b0070ec09b0297b796
Naid <61181979@continental.edu.pe>
2025-12-03 12:50:33 -0500

    Actualizacion

--- generador.py
+++ generador.py
@@ -3,14 +3,22 @@ from matplotlib.patches import FancyArrowPatch
 
 
 # === Definición del DFA ===
-states = {"q0","q1","q2"}
-alphabet = {"0","1"}
-delta = {("q0","1"):"q1",
-        ("q1","1"):"q2",
-        ("q2","1"):"q2",
-        ("q0","0"):"q1",
-        ("q1","0"):"q2",
-        ("q2","0"):"q2"}
+states = {"Insertar Tarjeta","Insertar PIN","Menu","Dollar", "Soles", "Transaccion", "Imprimir recibo", "Fin de operacion"}
+alphabet = {"a","b","c"}
+delta = {("Insertar Tarjeta","a"):"Insertar PIN",
+        ("Insertar Tarjeta","c"):"Insertar Tarjeta",
+        ("Insertar Tarjeta","b"):"Fin de operacion",
+        ("Insertar PIN","a"):"Menu",
+        ("Menu","c"):"Menu",
+        ("Menu","a"):"Soles",
+        ("Menu","a"):"Dollar",
+        ("Soles","b"):"Menu",
+        ("Dollar","b"):"Menu",
+        ("Soles","a"):"Transaccion",
+        ("Dollar","a"):"Transaccion",
+        ("Transaccion","a"):"Imprimir recibo",
+        ("Transaccion","b"):"Fin de operacion",
+        ("Imprimir recibo","a"):"Fin de operacion"}
 
 
 q0, F = "q0", {"q2"}
